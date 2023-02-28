38d5b8ffb3bb829ca502d3ced7ba195c0fa0df75
mostafamohammadi1989 <mostafa.mohammadi.1989.10.26@gmail.com>
2023-02-28 20:51:59 +0330

    Add color and define them in the theme file

---
+++ src/common/Theme.js
@@ -0,0 +1,22 @@
+export const Theme = {
+  colors: {
+    RoyalBlue: "#3763EB",
+    RoyalBlueLight: "#798fff",
+    RoyalBlueDark: "#0039b7",
+    Dolphin: "#6E657E",
+    DolphinLight: "#9c93ad",
+    DolphinDark: "#433b52",
+    BlackPearl: "#081828",
+    BlackPearlLight: "#303e50",
+    BlackPearlDark: "#000000",
+    DustyGray: "#999999",
+    DustyGrayLight: "#cacaca",
+    DustyGrayDark: "#6b6b6b",
+    BlackSqueeze: "#f6f9fc",
+    BlackSqueezeLight: "#ffffff",
+    BlackSqueezeDark: "#c3c6c9",
+  },
+  typography: {
+    fontSize: {},
+  },
+};
